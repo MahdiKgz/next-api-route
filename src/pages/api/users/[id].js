4f2e37d4707fa31495c4ed4b2b8589337184f39a
Mahdi Khoshghadam <Mahdikgz7@gmail.com>
2025-01-14 14:24:36 +0330

    Added search api

--- src/pages/api/users/[id].js
+++ src/pages/api/users/[id].js
@@ -1,15 +1,16 @@
-import { users } from "../auth"
+import { users } from "../auth";
 
-const handler = (req , res) => {
-    if (req.method === "GET") {
-        console.log(req.query)
-        const {id} = req.query
-        const selectedUser = users.find(user => user.id === +id)
-        if(!selectedUser) {
-            return res.status(404).json({message : `User with id : ${id} not found`})
-        }
-        return res.status(200).json({message : `requested User : ${selectedUser?.name}`})
+const handler = (req, res) => {
+  if (req.method === "GET") {
+    const { id } = req.query;
+    const selectedUser = users.find((user) => user.id === +id);
+    if (!selectedUser) {
+      return res
+        .status(404)
+        .json({ message: `User with id : ${id} not found` });
     }
-}
+    return res.status(200).json(selectedUser);
+  }
+};
 
-export default handler
+export default handler;
